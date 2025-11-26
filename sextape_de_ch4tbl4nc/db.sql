f01d4870e1d20fbb6d6a9e096308ec4fe8082c7c
k0nrd <zmurka10@gmail.com>
2025-11-26 17:39:16 +0100

    fichiers pour l'autre tyran (jme sens oppressé)

--- sextape_de_ch4tbl4nc/db.sql
+++ sextape_de_ch4tbl4nc/db.sql
@@ -3,19 +3,19 @@ CREATE DATABASE IF NOT EXISTS guardia_events CHARACTER SET utf8mb4 COLLATE utf8m
 USE guardia_events;
 
 CREATE TABLE IF NOT EXISTS events (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    postal_code VARCHAR(10) NOT NULL,
+    country VARCHAR(100) DEFAULT 'France',
 
-    name VARCHAR(255) NOT NULL,
-    address VARCHAR(255) NOT NULL,
-    city VARCHAR(50) NOT NULL,
-    postal_code VARCHAR(10) NOT NULL,
-    country VARCHAR(100) DEFAULT 'France',
+    description TEXT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-    description TEXT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
-    theme VARCHAR(25) NULL,
-    date DATE NOT NULL,
-    heure_debut TIME NOT NULL,
-    heure_fin TIME NOT NULL
+    theme VARCHAR(25) NULL,
+    date DATE NOT NULL,
+    heure_debut TIME NOT NULL,
+    heure_fin TIME NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
